69fbe507d24c11f56ee999d4b01557784ca48870
colinmollenhour <colin@mollenhour.com>
2012-04-19 11:56:41 -0400

    Add proper README file.

--- File.php
+++ File.php
@@ -2,31 +2,6 @@
 /**
  * Cm_Cache_Backend_File
  *
- * The stock Zend_Cache_Backend_File backend has extremely poor performance for
- * cleaning by tags making it become unusable as the number of cached items
- * increases. This backend make many changes resulting in a huge performance boost,
- * especially for tag cleaning.
- *
- * This cache backend works by indexing tags in files so that tag operations
- * do not require a full scan of every cache file. The ids are written to the
- * tag files in append-only mode and only when files exceed 4k and only randomly
- * are the tag files compacted to prevent endless growth in edge cases.
- *
- * The metadata and the cache record are stored in the same file rather than separate
- * files resulting in fewer inodes and fewer file stat/read/write/lock/unlink operations.
- * Also, the original hashed directory structure had very poor distribution due to
- * the adler32 hashing algorithm and prefixes. The multi-level nested directories
- * have been dropped in favor of single-level nesting made from multiple characters.
- *
- * Benchmark tool here: https://github.com/colinmollenhour/magento-cache-benchmark
- *
- * Installation:
- *  1. Clone module with modman
- *  2. Edit app/etc/local.xml changing global/cache/backend to "Cm_Cache_Backend_File"
- *  3. Delete all contents of cache directory
- *
- * Thanks to Vinai Kopp for the inspiring this backend with your symlink rendition!
- *
  * @copyright  Copyright (c) 2012 Colin Mollenhour (http://colin.mollenhour.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */

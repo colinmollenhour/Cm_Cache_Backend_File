5797f6d04c9e9bb55ce23d9f3686104961b25c4b
Colin Mollenhour <colin@mollenhour.com>
2017-12-05 23:13:19 -0500

    Quiet wget.

--- travis_build.sh
+++ travis_build.sh
@@ -11,26 +11,26 @@ require_once 'File.php';
 mkdir -p Zend/Cache/Backend
 mkdir -p Zend/Log/{Filter,Formatter,Writer}
 cd $root/Zend
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Exception.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Cache.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Exception.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Cache.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log.php
 cd $root/Zend/Cache
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Exception.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Exception.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend.php
 cd $root/Zend/Cache/Backend
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend/Interface.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend/ExtendedInterface.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend/File.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend/Interface.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend/ExtendedInterface.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Cache/Backend/File.php
 cd $root/Zend/Log
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/FactoryInterface.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/FactoryInterface.php
 cd $root/Zend/Log/Filter
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Filter/Priority.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Filter/Abstract.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Filter/Interface.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Filter/Priority.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Filter/Abstract.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Filter/Interface.php
 cd $root/Zend/Log/Formatter
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Formatter/Simple.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Formatter/Abstract.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Formatter/Interface.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Formatter/Simple.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Formatter/Abstract.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Formatter/Interface.php
 cd $root/Zend/Log/Writer
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Writer/Stream.php
-wget https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Writer/Abstract.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Writer/Stream.php
+wget -q https://github.com/zendframework/zf1/raw/master/library/Zend/Log/Writer/Abstract.php
